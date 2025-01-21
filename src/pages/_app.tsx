fbe5243330768270b66f832a2619f1d2ab0c3f9b
Olzhas Nurpeisov <olzhik1123@gmail.com>
2025-01-21 11:22:28 +0900

    add OTA delivery for web app

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,13 +1,34 @@
 import "@/styles/globals.css";
 import type { AppProps } from "next/app";
 import translations from "../locale/home";
-import { LocaleProvider } from "@/components/providers/LocaleProvider";
+import {
+  Language,
+  LocaleProvider,
+} from "@/components/providers/LocaleProvider";
 import TranslationsProvider from "@/components/providers/TranslationsProvider";
+import OtaClient from "@crowdin/ota-client";
+import { getTranslations } from "@/utils";
+import { useEffect, useState } from "react";
 
 export default function App({ Component, pageProps }: AppProps) {
+  const client = new OtaClient(process.env.NEXT_PUBLIC_DISTRIBUTION_HASH!);
+
+  const [strings, setStrings] = useState<
+    Record<Language, Record<string, string>>
+  >({
+    ko: {},
+    en: {},
+  });
+
+  useEffect(() => {
+    getTranslations(client, translations).then((translations) => {
+      setStrings({ ...translations, en: translations.en });
+    });
+  }, []);
+
   return (
     <LocaleProvider>
-      <TranslationsProvider translations={translations}>
+      <TranslationsProvider translations={strings}>
         <Component {...pageProps} />
       </TranslationsProvider>
     </LocaleProvider>
